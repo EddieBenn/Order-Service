47f91cf9312ee0c85664d5e3551487bb8870006e
Edidiong Ndaobong <edidiong.ndaobong@maxdrive.ai>
2024-12-04 21:10:15 +0100

    customer implementation started

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,16 @@ import { AppService } from './app.service';
 import dotenv from 'dotenv';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { CustomersModule } from './customers/customers.module';
 
 dotenv.config();
 
 @Module({
-  imports: [MongooseModule.forRoot(process.env.MONGO_URI), ConfigModule],
+  imports: [
+    MongooseModule.forRoot(process.env.MONGO_URI),
+    ConfigModule,
+    CustomersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
